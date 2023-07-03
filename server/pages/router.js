746617c6da084d2a9bd86f874d45e95eebe71887
Senalov <senalovaziz12@gmail.com>
2023-07-03 18:01:22 +0600

    add signout

--- server/pages/router.js
+++ server/pages/router.js
@@ -4,31 +4,31 @@ const Genres = require('../Genres/genres')
 
 router.get('/' , async(req, res) => {
     const allGenres = await Genres.find()
-    res.render("index" , {genres: allGenres})
+    res.render("index" , {genres: allGenres , user: req.user ? req.user : {}})
 })
 
 router.get('/login' , (req, res) => {
-    res.render("login")
+    res.render("login" , {user: req.user ? req.user : {}})
 })
 
 router.get('/register' , (req, res) => {
-    res.render("register")
+    res.render("register" , {user: req.user ? req.user : {}})
 })
 
-router.get('/profile' , (req, res) => {
-    res.render("profile")
+router.get('/profile/:id' , (req, res) => {
+    res.render("profile" , {user: req.user ? req.user : {}})
 })
 
 router.get('/admin' , (req, res) => {
-    res.render("adminProfile")
+    res.render("adminProfile" , {user: req.user ? req.user : {}})
 })
 
 router.get('/new' , (req, res) => {
-    res.render("newFilm")
+    res.render("newFilm" , {user: req.user ? req.user : {}})
 })
 
 router.get('/edit' , (req, res) => {
-    res.render("editFilm")
+    res.render("editFilm" , {user: req.user ? req.user : {}})
 })
 
 module.exports = router
