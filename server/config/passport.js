3ba424243d0eb014c06acfbf314b3233d6d2dc54
Senalov <senalovaziz12@gmail.com>
2023-08-04 23:14:10 +0600

    finished the block with registration using google

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,7 +2,9 @@ const passport = require('passport')
 const User = require('../auth/User')
 const bcrypt = require('bcrypt')
 const localStrategy = require('passport-local')
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+// CLIENT_ID: 96184516550-8rh2jfic210jr43s07m8vrtcopu7fbiq.apps.googleusercontent.com
+// CLIENT_SECRET: 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
 passport.use(new localStrategy(
     {
         usernameField: 'email'
@@ -22,6 +24,23 @@ passport.use(new localStrategy(
         })
     }
 ))
+
+passport.use(new GoogleStrategy({
+    clientID: '96184516550-8rh2jfic210jr43s07m8vrtcopu7fbiq.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: "http://localhost:1000/api/auth/google",
+    scope: ['openid' , 'email' , 'profile']
+  },
+  async function(accessToken, refreshToken, profile, cb) {
+    const user = await User.find({ googleId: profile.id })
+    const newUser = await new User({
+        googleId: profile.id,
+        full_name: profile.displayName,
+        email: profile.emails[0].value
+    }).save()
+    return cb(null, newUser);
+  }
+));
 
 passport.serializeUser(function(user, done){
     done(null , user._id)
